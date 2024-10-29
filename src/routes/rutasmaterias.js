4ed8b2c218cee489b1801daa1fdc036325c27d9a
edancabreraav <edancabreraav@ittepic.edu.mx>
2024-10-29 16:46:15 -0700

    cambio menor

--- src/routes/rutasmaterias.js
+++ src/routes/rutasmaterias.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const materiaController = require('../controller/materiaController');
+const materiaController = require('../controllers/materiaController');
 const cache = require('./cache');
 
 // Ruta para crear una materia
