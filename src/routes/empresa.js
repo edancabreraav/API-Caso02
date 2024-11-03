92d5e932438f1be79dc0f95c567a454f74e52fe6
cabrera <edancabreraav@ittepic.edu.mx>
2024-11-03 13:45:22 -0700

    DELETE 1 y 2 incompletos
    Se agregaron dos DELETE a empresaController.js, falta revisarlos.

--- src/routes/empresa.js
+++ src/routes/empresa.js
@@ -13,4 +13,10 @@ router.get('/internacional/paises_no_opera/:rfc', /*cache,*/ empresaController.g
 //Ruta para obtener las empresas que tiene más de 10 aviones y que trabajan en al menos dos aeropuertos diferentes
 router.get('/diezoMasAviones/dosoMasAeropuertos', /*cache,*/ empresaController.getAvionesAeropuertos);
 
+//Ruta para eliminar una empresa que ya no trabaja en un aeropuerto específico y eliminar también la información asociada
+router.delete('/relacionAeropuerto', empresaController.deleteEmpresaFromAeropuerto);
+
+//Ruta para dar de baja a todos los pilotos de una empresa
+router.delete('/bajaPilotos', empresaController.deletePilotos);
+
 module.exports = router;
