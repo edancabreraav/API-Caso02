71e85d4f5fd4e49c78777d1916a00331c1ada5c1
edancabreraav <edancabreraav@ittepic.edu.mx>
2024-11-03 18:05:09 -0700

    GET 6/6, POST 2/2, PUT 1/2, DELETE 2/2

--- src/controllers/aeropuertoController.js
+++ src/controllers/aeropuertoController.js
@@ -1,5 +1,6 @@
 const neo4jDriver = require('../models/neo4jDatabase');
 
+//----------GET-----------
 //Función obtener la lista de aeropuertos con más de 3 pistas
 exports.getAeropuertosPistas = async (req, res) => {
     const minPistas = parseInt(req.params.minPistas);
@@ -17,6 +18,30 @@ exports.getAeropuertosPistas = async (req, res) => {
     } catch (error) {
         console.error('Error al consultar pistas', error);
         res.status(500).json({ message: 'Error al consultar pistas', error });
+    } finally {
+        await session.close();
+    }
+};
+
+//----------PUT----------
+//Función cambiar el numero de pistas de un aeropuerto
+exports.putPistas= async (req, res) => {
+    const id_aeropuerto = parseInt(req.params.id_aeropuerto);
+    const pistas = req.body.pistas;
+
+    const query = `
+        MATCH (a:Aeropuerto {id_aeropuerto:$id_aeropuerto}) SET a.numero_de_pistas= $pistas RETURN a
+    `;
+    const session = neo4jDriver.session();
+
+    try {
+        const result = await session.run(query, { id_aeropuerto, pistas});
+        const aeropuerto = result.records.map(record => record.get('a').properties);
+        res.data = aeropuerto;
+        res.json({ Aeropuertos: aeropuerto });
+    } catch (error) {
+        console.error('Error al actualizar número de pistas', error);
+        res.status(500).json({ message: 'Error al actualizar número de pistas', error });
     } finally {
         await session.close();
     }
--- src/routes/aeropuerto.js
+++ src/routes/aeropuerto.js
@@ -6,4 +6,7 @@ const cache = require('./cache');
 // Ruta para obtener la lista de aeropuertos con más de 3 pistas
 router.get('/pistas/:minPistas', /*cache,*/ aeropuertoController.getAeropuertosPistas);
 
+//Ruta para cambiar el número de pistas de un aeropuerto
+router.put('/actualizarPistas/:id_aeropuerto', aeropuertoController.putPistas)
+
 module.exports = router;
